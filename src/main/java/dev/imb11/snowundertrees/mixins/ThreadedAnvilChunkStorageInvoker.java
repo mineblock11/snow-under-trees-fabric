e4538a947b9298f2cfc15eeb1a85bee9d105a8d6
Calum <contact@mineblock11.dev>
2024-06-23 22:10:32 +0100

    Why

--- src/main/java/dev/imb11/snowundertrees/mixins/ThreadedAnvilChunkStorageInvoker.java
+++ src/main/java/dev/imb11/snowundertrees/mixins/ThreadedAnvilChunkStorageInvoker.java
@@ -1,14 +1,13 @@
 package dev.imb11.snowundertrees.mixins;
 
 import net.minecraft.server.world.ChunkHolder;
-import net.minecraft.server.world.ServerChunkLoadingManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 /*? if <1.21 {*/
 /*@Mixin(net.minecraft.server.world.ThreadedAnvilChunkStorage.class)
 *//*?} else {*/
-@Mixin(ServerChunkLoadingManager.class)
+@Mixin(net.minecraft.server.world.ServerChunkLoadingManager.class)
 /*?}*/
 public interface ThreadedAnvilChunkStorageInvoker {
     @Invoker("entryIterator")
